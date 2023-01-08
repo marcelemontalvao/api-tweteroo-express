fbb67db8429a911839af0dd2dc7dd430fbeba4f2
marcelemontalvao <mmontalvaoblog@outlook.com>
2023-01-08 19:35:59 -0300

    endpoint /sign-up

--- src/app.js
+++ src/app.js
@@ -3,19 +3,33 @@ import cors from "cors";
 
 const server = express();
 
+server.use(express.json())
 server.use(cors());
 
 server.listen("5000", () => {
     console.log("servidor executado com sucesso")
 })
 
-server.get("/tweets", (request, response) => {
-    const tweets = [
-        {
-            username: "bobesponja",
-            avatar: "https://super.abril.com.br/wp-content/uploads/2020/09/04-09_gato_SITE.jpg?quality=70&strip=info",
-            tweet: "eu amo o hub"
-        }
-    ]
-    response.send(tweets)
+const users = []
+
+const tweets = []
+
+server.post("/sign-up", (request, response) => {
+    const { username, avatar } = request.body
+
+	if (!username) {
+	    response.send('Preencha seu nome!');
+        return;
+	} else if (!avatar){
+        response.send('Preencha seu avatar!');
+        return;
+    }
+
+    const user = {
+      username: username,
+      avatar: avatar
+    }
+
+    users.push(user)
+    response.send("OK")
 })
